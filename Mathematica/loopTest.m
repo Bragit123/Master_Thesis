337b6850e7f977498a75f3d0ae8e022b82583782
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2025-10-13 17:03:29 +0200

    Minor update

---
+++ Mathematica/loopTest.m
@@ -0,0 +1,5 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+(FeynCalc`A0[MSf[A, 2, i]^2]*IndexDelta[A, B]*SMP["e"]^2)/(16*Pi^2) - 
+ (FeynCalc`B0[Pair[Momentum[p, D], Momentum[p, D]], 0, MSf[A, 2, i]^2]*
+   IndexDelta[A, B]*(MSf[A, 2, i]^2 + Pair[Momentum[p, D], Momentum[p, D]])*
+   SMP["e"]^2)/(8*Pi^2)

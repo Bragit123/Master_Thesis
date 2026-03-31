8561cf0a46a3381ce06689e5993d63b627bf3a79
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2026-03-31 19:41:02 +0200

    Tested LHAPDF to obtain central PDF and plot for the up quark.

---
+++ Code/PDF_integration/plot.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+sns.set_theme()
+# sns.set_context("paper")
+
+file_path = "output/PDF4LHC21_40_0_2.dat"
+df = pd.read_csv(file_path, sep=" ", names=["x","Q2","xf"])
+
+
+## Plot using seaborn
+q2_values = df["Q2"].unique()[::100]
+df_filtered = df[df["Q2"].isin(q2_values)]
+
+nQ2 = df["Q2"].nunique()
+
+fig, ax = plt.subplots(figsize=(8,6))
+fig.suptitle("PDF $x f_u(x,Q^2)$")
+sns.lineplot(
+  ax=ax,
+  data=df,
+  x="x",
+  y="xf",
+  hue="Q2",
+  palette=sns.color_palette("crest", nQ2),
+  legend=False
+)
+
+## Add colorbar
+q2min = df["Q2"].unique().min()
+q2max = df["Q2"].unique().max()
+norm = mcolors.LogNorm(vmin=q2min, vmax=q2max)
+sm = cm.ScalarMappable(cmap="crest", norm=norm)
+fig.colorbar(sm, ax=ax, label="Q2")
+
+ax.set(
+  xscale="log",
+  xlabel="$x$",
+  ylabel="$xf$"
+)
+fig.savefig("pdf.pdf")
--- Code/PDF_integration/src/main.cpp
+++ Code/PDF_integration/src/main.cpp
@@ -1,7 +1,55 @@
+#include "LHAPDF/LHAPDF.h"
 #include <iostream>
+#include <fstream>
+
+using string = std::string;
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello World" << std::endl;
+    
+  if (argc < 3) {
+		std::cerr << "You must specify a PDF set and member number." << std::endl;
+		return 1;
+	}
 
-    return 0;
+	// Extract PDF set name and member number from command line input
+	const string setname = argv[1];
+	const string smem = argv[2];
+	const int imem = LHAPDF::lexical_cast<int>(smem);
+
+	const LHAPDF::PDF* pdf = LHAPDF::mkPDF(setname, imem);
+	std::vector<int> pids = pdf->flavors();
+
+	const double MINLOGX = -10;
+	const double MAXLOGX = 0;
+	const double DX = 0.01;
+	const int NX = (int) std::floor((MAXLOGX - MINLOGX)/DX) + 1;
+
+	const double MINLOGQ2 = 1;
+	const double MAXLOGQ2 = 8;
+	const double DQ2 = 0.01;
+	const int NQ2 = (int) std::floor((MAXLOGQ2 - MINLOGQ2)/DQ2) + 1;
+
+	const string OUTPUT_DIR = "output/";
+	for (int pid : pids) {
+		const string spid = LHAPDF::lexical_cast<string>(pid);
+		const string filename = OUTPUT_DIR + setname + "_" + smem + "_" + spid + ".dat";
+
+		std::ofstream out_file(filename);
+		for (int ix=0; ix < NX; ++ix) {
+			// If log10x is very close to 0, set it to 0
+			const double log10x = (MINLOGX + ix*DX < -1e-3) ? (MINLOGX + ix*DX) : 0;
+			const double x = std::pow(10, log10x);
+			for (int iq2=0; iq2 < NQ2; ++iq2) {
+				const double log10q2 = MINLOGQ2 + iq2*DQ2;
+				const double q2 = std::pow(10, log10q2);
+				const double xf = pdf->xfxQ2(pid, x, q2);
+
+				out_file << x << " " << q2 << " "  << xf << std::endl;
+			}
+		}
+		out_file.close();
+	}
+
+	delete pdf;
+	return 0;
 }

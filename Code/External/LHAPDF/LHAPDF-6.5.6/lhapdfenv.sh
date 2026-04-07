7058431a47e96eaea396c157e1f19d04e602e8c7
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2026-04-07 13:54:09 +0200

    Stopped tracking External/LHAPDF.

--- Code/External/LHAPDF/LHAPDF-6.5.6/lhapdfenv.sh
+++
@@ -1,9 +0,0 @@
-# convenience variables that must exist
-prefix=/Users/brageandreastrefjord/Desktop/Github/Master_Thesis/Code/External/LHAPDF/LHAPDF-6.5.6/..
-exec_prefix=${prefix}
-
-export PATH="$exec_prefix/bin":$PATH
-export PYTHONPATH="/Users/brageandreastrefjord/Desktop/Github/Master_Thesis/Code/External/LHAPDF/LHAPDF-6.5.6/../lib/python3.14/site-packages:$PYTHONPATH"
-export DYLD_LIBRARY_PATH="${exec_prefix}/lib:$DYLD_LIBRARY_PATH"
-export LHAPDF_DATA_PATH="$prefix/share/LHAPDF/"
-
